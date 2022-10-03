9d66c41df756c67c9493609ca781e48daea54803
José Zambrano <zambranomoya74@gmail.com>
2022-10-03 17:06:48 +0200

    Lazy retrasar creación adapter

---
+++ app/src/main/java/com/example/myapp01/Listener.kt
@@ -0,0 +1,9 @@
+package com.example.myapp01
+
+/****
+ * Project: My app01
+ * From: com.example.myapp01
+ * Created by José Zambrano Moya on 3/10/22 at 16:19
+ * More info: zambranomoya74@gmail.com
+ ****/interface Listener {
+}
